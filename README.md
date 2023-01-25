b6ae693e4a11709ca650ce5ffaf333ee035c1092
Ben Lu <ayroblu@gmail.com>
2023-01-25 11:54:52 +0000

    More benchmarks

--- README.md
+++ README.md
@@ -7,5 +7,9 @@ What's the cost of virtual dom v real dom?
   - 520ms
 - With fragment:
   - 320ms
+- With direct (`Renderer()` vs `<Renderer />`):
+  - 180ms
+- With direct no fragment:
+  - 9ms
 
 Measurement using performance recorder
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
 const isDiv = false;
+const isDirect = false;
+const isDirectNoFragment = true;
 function App() {
   const [count, setCount] = useState(0)
 
@@ -13,12 +15,16 @@ function App() {
     </div>
   )
 }
-function Renderer({count}: {count: number}) {
+function Renderer({count}: {count: number}): JSX.Element {
   if (count > 1000) {
     return <div>Content</div>
   }
   if (isDiv) {
     return <div><Renderer count={count+1}/></div>
+  } else if (isDirect) {
+    return <>{Renderer({count: count+1})}</>
+  } else if (isDirectNoFragment) {
+    return Renderer({count: count+1})
   } else {
     return <><Renderer count={count+1}/></>
   }
